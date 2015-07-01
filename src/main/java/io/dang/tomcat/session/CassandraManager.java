4852660166a61dacde27af22e15dbfbc8753813a
jcdang <jcdang@gmail.com>
2015-06-30 20:49:21 -0500

    added getters and setters

--- src/main/java/io/dang/tomcat/session/CassandraManager.java
+++ src/main/java/io/dang/tomcat/session/CassandraManager.java
@@ -10,11 +10,21 @@ import org.apache.catalina.session.PersistentManagerBase;
 public class CassandraManager extends PersistentManagerBase {
     protected static final String NAME = "CassandraManager";
 
+
+
+
+    protected String clusterName;
     protected String keyspace;
     protected String tableName;
+    protected String nodes;
 
     public CassandraManager() {
 
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
     }
 
     @Override
@@ -25,6 +35,38 @@ public class CassandraManager extends PersistentManagerBase {
     @Override
     public String generateSessionId() {
         return UUIDs.timeBased().toString();
+    }
+
+    public String getClusterName() {
+        return clusterName;
+    }
+
+    public void setClusterName(String clusterName) {
+        this.clusterName = clusterName;
+    }
+
+    public String getKeyspace() {
+        return keyspace;
+    }
+
+    public void setKeyspace(String keyspace) {
+        this.keyspace = keyspace;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public String getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(String nodes) {
+        this.nodes = nodes;
     }
 
 }
